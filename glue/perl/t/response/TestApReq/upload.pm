1ca1b1cc46c678ec619eba747f1af71748af5260
randyk <randyk@unknown>
2004-07-12 13:10:14 +0000

    Reviewed by:	joes add upload test for the perl glue involving uploading text and binary files.
    git-svn-id: https://svn.apache.org/repos/asf/httpd/apreq/trunk@104245 13f79535-47bb-0310-9956-ffa450edef68

---
+++ glue/perl/t/response/TestApReq/upload.pm
@@ -0,0 +1,88 @@
+package TestApReq::upload;
+
+use strict;
+use warnings FATAL => 'all';
+
+use Apache::RequestRec;
+use Apache::RequestIO;
+use Apache::Request ();
+use Apache::Upload;
+use File::Spec;
+require File::Basename;
+
+sub handler {
+    my $r = shift;
+    my $req = Apache::Request->new($r);
+    my $temp_dir = File::Spec->tmpdir;
+
+    my $method  = $req->args('method');
+    my $has_md5  = $req->args('has_md5');
+    require Digest::MD5 if $has_md5;
+    my $upload = $req->upload(($req->upload)[0]);
+    my $type = $upload->type;
+    my $basename = File::Basename::basename($upload->filename);
+    my ($data, $fh);
+
+    if ($method eq 'slurp') {
+        $upload->slurp($data);
+    }
+    elsif ($method eq 'fh') {
+        read $upload->fh, $data, $upload->size;
+    }
+    elsif ($method eq 'tempname') {
+        my $name = $upload->tempname;
+        open $fh, "<:APR", $name, $upload->pool 
+            or die "Can't open $name: $!";
+        binmode $fh;
+        read $fh, $data, $upload->size;
+        close $fh;
+    }
+    elsif ($method eq 'link') {
+        my $link_file = File::Spec->catfile($temp_dir, "linkfile");
+        unlink $link_file if -f $link_file;
+        $upload->link($link_file) or die "Can't link to $link_file: $!";
+        open $fh, "<", $link_file or die "Can't open $link_file: $!";
+        binmode $fh;
+        read $fh, $data, $upload->size;
+        close $fh;
+        unlink $link_file if -f $link_file;
+    }
+    elsif ($method eq 'io') {
+       read $upload->io, $data, $upload->size;
+    }
+    else  {
+        die "unknown method: $method";
+    }
+
+    my $temp_file = File::Spec->catfile($temp_dir, $basename);
+    unlink $temp_file if -f $temp_file;
+    open my $wfh, ">", $temp_file or die "Can't open $temp_file: $!";
+    binmode $wfh;
+    print $wfh $data;
+    close $wfh;
+    my $cs = $has_md5 ? cs($temp_file) : 0;
+ 
+    $req->content_type('text/plain');
+    my $size = -s $temp_file;
+    $r->print(<<END);
+
+type: $type
+size: $size
+filename: $basename
+md5: $cs
+END
+    unlink $temp_file if -f $temp_file;
+    return 0;
+}
+
+sub cs {
+    my $file = shift;
+    open my $fh, '<', $file or die qq{Cannot open "$file": $!};
+    binmode $fh;
+    my $md5 = Digest::MD5->new->addfile($fh)->hexdigest;
+    close $fh;
+    return $md5;
+}
+
+1;
+__END__
