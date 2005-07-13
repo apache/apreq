477e8277e37a37f4754de17a3754c86f1e622aa0
Joe Schaefer <joes@apache.org>
2005-07-13 17:41:22 +0000

    Fix "Attempt to free temp prematurely" bug in apreq_xs_error(). The reason is that Perl_load_module() "magically frees" the two SVs; notably pkg_name.
    Submitted by: Philip M. Gollucci
    Message-ID: <42D4C392.3030708@p6m7g8.com>
    Reviewed by: joes
    
    
    git-svn-id: https://svn.apache.org/repos/asf/httpd/apreq/trunk@216197 13f79535-47bb-0310-9956-ffa450edef68

--- glue/perl/xsbuilder/apreq_xs_postperl.h
+++ glue/perl/xsbuilder/apreq_xs_postperl.h
@@ -243,7 +243,7 @@ void apreq_xs_croak(pTHX_ HV *data, SV *obj, apr_status_t rc,
 
     stash = gv_stashpv(ERROR_CLASS, FALSE);
     if (stash == NULL) {
-        SV *pkg_name = sv_2mortal(newSVpv(class, 0));
+        SV *pkg_name = newSVpv(class, 0);
         Perl_load_module(aTHX_ PERL_LOADMOD_NOIMPORT, pkg_name, Nullsv);
         stash = gv_stashpv(class, TRUE);
     }
