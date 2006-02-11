f40bdffcd3c159f3405009c3103398cf01ad7376
Joe Schaefer <joes@apache.org>
2006-02-11 16:31:28 +0000

    on to 2.08
    git-svn-id: https://svn.apache.org/repos/asf/httpd/apreq/trunk@377000 13f79535-47bb-0310-9956-ffa450edef68

--- glue/perl/lib/Apache2/Cookie.pm
+++ glue/perl/lib/Apache2/Cookie.pm
@@ -9,7 +9,7 @@ use Apache2::RequestUtil;
 use overload '""' => sub { shift->as_string() }, fallback => 1;
 
 push our @ISA, "APR::Request::Cookie";
-our $VERSION = "2.07";
+our $VERSION = "2.08";
 
 sub new {
     my ($class, $r, %attrs) = @_;
