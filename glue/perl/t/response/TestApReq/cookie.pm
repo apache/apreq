1ee9a4c208a202a3fe5ee1c1640192d214511603
Philip M. Gollucci <pgollucci@apache.org>
2006-08-03 10:08:27 +0000

    o don't import anything o Apache2::Connection isn't needed here
    git-svn-id: https://svn.apache.org/repos/asf/httpd/apreq/trunk@428299 13f79535-47bb-0310-9956-ffa450edef68

--- glue/perl/t/response/TestApReq/cookie.pm
+++ glue/perl/t/response/TestApReq/cookie.pm
@@ -3,9 +3,8 @@ package TestApReq::cookie;
 use strict;
 use warnings FATAL => 'all';
 
-use Apache2::RequestIO;
-use Apache2::RequestRec;
-use Apache2::Connection;
+use Apache2::RequestIO ();
+use Apache2::RequestRec ();
 
 use Apache2::Cookie ();
 use Apache2::Request ();
