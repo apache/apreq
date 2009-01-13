16a42c13ea9d7a8ac6d9f1bcde330e1234114161
Joe Schaefer <joes@apache.org>
2009-01-13 16:22:44 +0000

    fix update_version.pl script
    git-svn-id: https://svn.apache.org/repos/asf/httpd/apreq/trunk@734170 13f79535-47bb-0310-9956-ffa450edef68

--- glue/perl/lib/Apache2/Upload.pm
+++ glue/perl/lib/Apache2/Upload.pm
@@ -1,7 +1,7 @@
 package Apache2::Upload;
 use Apache2::Request;
 push our @ISA, qw/APR::Request::Param/;
-our $VERSION = "2.08";
+our $VERSION = "2.11";
 no strict 'refs';
 for (qw/slurp type size link tempname fh io filename/) {
     *{$_} = *{"APR::Request::Param::upload_$_"}{CODE};
