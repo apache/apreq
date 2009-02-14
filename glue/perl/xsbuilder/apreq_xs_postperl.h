88226c376fb59d15a2a6eeae6f15631c86276be1
Joe Schaefer <joes@apache.org>
2009-02-14 04:57:51 +0000

    free pkg_name by decrementing the refcnt instead of making it mortal
    git-svn-id: https://svn.apache.org/repos/asf/httpd/apreq/trunk@744390 13f79535-47bb-0310-9956-ffa450edef68

--- glue/perl/xsbuilder/apreq_xs_postperl.h
+++ glue/perl/xsbuilder/apreq_xs_postperl.h
@@ -247,6 +247,7 @@ void apreq_xs_croak(pTHX_ HV *data, SV *obj, apr_status_t rc,
         SV *pkg_name = newSVpv(class, 0);
         Perl_load_module(aTHX_ PERL_LOADMOD_NOIMPORT, pkg_name, Nullsv);
         stash = gv_stashpv(class, TRUE);
+        SvREFCNT_dec(pkg_name);
     }
 
     if (obj != Nullsv)
