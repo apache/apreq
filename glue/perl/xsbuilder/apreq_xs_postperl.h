342a02cca850e29124e07b2f5dd58e16becac6d4
Joe Schaefer <joes@apache.org>
2009-02-14 05:43:50 +0000

    ugh, looking over the perl source it seems we need an ENTER & LEAVE block around Perl_load_module
    git-svn-id: https://svn.apache.org/repos/asf/httpd/apreq/trunk@744402 13f79535-47bb-0310-9956-ffa450edef68

--- glue/perl/xsbuilder/apreq_xs_postperl.h
+++ glue/perl/xsbuilder/apreq_xs_postperl.h
@@ -244,10 +244,11 @@ void apreq_xs_croak(pTHX_ HV *data, SV *obj, apr_status_t rc,
 
     stash = gv_stashpv(class, FALSE);
     if (stash == NULL) {
-        SV *pkg_name = newSVpv(class, 0);
-        Perl_load_module(aTHX_ PERL_LOADMOD_NOIMPORT, pkg_name, Nullsv);
+        ENTER;
+        Perl_load_module(aTHX_ PERL_LOADMOD_NOIMPORT,
+                         newSVpv(class, 0), Nullsv);
+        LEAVE;
         stash = gv_stashpv(class, TRUE);
-        SvREFCNT_dec(pkg_name);
     }
 
     if (obj != Nullsv)
