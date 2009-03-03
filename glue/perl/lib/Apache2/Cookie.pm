100fa5e6f6b6b0529791890776965ecf284529c0
Joe Schaefer <joes@apache.org>
2009-03-03 04:01:12 +0000

    bump package version
    git-svn-id: https://svn.apache.org/repos/asf/httpd/apreq/trunk@749522 13f79535-47bb-0310-9956-ffa450edef68

--- glue/perl/lib/Apache2/Cookie.pm
+++ glue/perl/lib/Apache2/Cookie.pm
@@ -9,7 +9,7 @@ use Apache2::RequestUtil;
 use overload '""' => sub { shift->as_string() }, fallback => 1;
 
 push our @ISA, "APR::Request::Cookie";
-our $VERSION = "2.11";
+our $VERSION = "2.12";
 
 sub new {
     my ($class, $r, %attrs) = @_;
