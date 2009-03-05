186efeaf815b16b06aa0f681296f10e4843422e2
Joe Schaefer <joes@apache.org>
2009-03-05 12:59:56 +0000

    more Win32 fu
    git-svn-id: https://svn.apache.org/repos/asf/httpd/apreq/trunk@750449 13f79535-47bb-0310-9956-ffa450edef68

--- module/apache2/apreq_module_apache2.h
+++ module/apache2/apreq_module_apache2.h
@@ -145,8 +145,13 @@ APREQ_DECLARE(apreq_handle_t *) apreq_handle_apache2(request_rec *r);
  *
  *      
  */
-APR_DECLARE_OPTIONAL_FN(APREQ_DECLARE(apreq_handle_t *),
+#ifdef WIN32
+APR_DECLARE_OPTIONAL_FN(apreq_handle_t * __stdcall,
                         apreq_handle_apache2, (request_rec *r));
+#else
+APR_DECLARE_OPTIONAL_FN(apreq_handle_t *,
+                        apreq_handle_apache2, (request_rec *r));
+#endif
 
 /**
  * The mod_apreq2 filter is named "apreq2", and may be used in Apache's
