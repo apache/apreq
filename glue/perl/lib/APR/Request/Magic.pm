d53be5c8e11368531515bf34265a964e722948da
Joe Schaefer <joes@apache.org>
2011-02-05 18:41:21 +0000

    unnecessary remarks
    git-svn-id: https://svn.apache.org/repos/asf/httpd/apreq/trunk@1067480 13f79535-47bb-0310-9956-ffa450edef68

--- glue/perl/lib/APR/Request/Magic.pm
+++ glue/perl/lib/APR/Request/Magic.pm
@@ -19,17 +19,11 @@ else {
 
 # Notes:
 #
-# 1) module authors should accept an APR::Request object in their
-#    args to new() instead of pulling out an apreq handle internally.
-#    the reason is that in a cgi context you'll need to share the
-#    pool with your users if they are to use apreq outside of your module,
-#    which is probably more trouble than it's worth.
-#
-# 2) the way to use this module is trivial:
+# 1) the way to use this module is trivial:
 #
 #    use APR::Request::Magic;
 #
 #    my $apreq = APR::Request::Magic->handle(@typical_args_sans_the_first_one);
 #    # do stuff with $apreq which is an APR::Request object
 #
-# 3) Be sure PerlOptions +GlobalRequest is set for mp2.
+# 2) Be sure PerlOptions +GlobalRequest is set for mp2.
