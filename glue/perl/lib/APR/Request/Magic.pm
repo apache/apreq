b49f0bd7a0831ad7e87a3deaa7e91c92b3f65fde
Joe Schaefer <joes@apache.org>
2011-02-05 05:19:49 +0000

    bugs
    git-svn-id: https://svn.apache.org/repos/asf/httpd/apreq/trunk@1067392 13f79535-47bb-0310-9956-ffa450edef68

--- glue/perl/lib/APR/Request/Magic.pm
+++ glue/perl/lib/APR/Request/Magic.pm
@@ -5,14 +5,14 @@ if ($@) {
     require APR::Request::CGI;
     require APR::Pool;
     base->import("APR::Pool");
-    *handle = *APR::Request::CGI::handle;
-    *new = sub { bless APR::Pool->new, shift; };
+    *handle = sub { APR::Request::CGI->handle(@_) };
+    *new = sub { bless APR::Pool->new, shift };
     return 1;
 }
 require Apache2::RequestRec;
 require Apache2::RequestUtil;
 base->import("Apache2::RequestRec");
-*handle = *APR::Request::Apache2::handle;
-*new = sub { bless Apache2::RequestUtil->request, shift; }
+*handle = sub { APR::Request::Apache2->handle(@_) };
+*new = sub { bless Apache2::RequestUtil->request, shift };
 
 1;
