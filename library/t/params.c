045d37c12dc03a4941842713aef8f13979f884c0
Joe Orton <jorton@apache.org>
2022-08-17 13:28:26 +0000

    Revert r1903490 since it makes the test fail even if the tests are individually successful (test number ordering fails).
    git-svn-id: https://svn.apache.org/repos/asf/httpd/apreq/trunk@1903492 13f79535-47bb-0310-9956-ffa450edef68

--- library/t/params.c
+++ library/t/params.c
@@ -104,8 +104,6 @@ static void header_attributes(dAT, void *ctx)
     apr_size_t vlen;
     apr_status_t s;
 
-    AT_localize();
-
     s = apreq_header_attribute(hdr, "none", 4, &val, &vlen);
     AT_int_eq(s, APREQ_ERROR_NOATTR);
 
@@ -134,8 +132,6 @@ static void header_attributes(dAT, void *ctx)
 
     s = apreq_header_attribute(hdr, "no-quote", 8, &val, &vlen);
     AT_int_eq(s, APREQ_ERROR_BADSEQ);
-
-    AT_delocalize();
 }
 
 
